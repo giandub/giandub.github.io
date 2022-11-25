55f11922f92f067f50d9d2de9d18ec55823d88fd
Gianfranco Di Pede <giandub@gmail.com>
2022-11-25 16:02:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-import 'arjs/aframe/build/aframe-ar-nft.js'
 
-AFRAME.registerComponent('clicker', {
-    init: function() {
-        this.el.addEventListener('click', e => {
-            alert('Box clicked!');
-        });
-    }
-});
